2cab4a7c9edaa482924c1b2d37e8619e3fa1272f
DouglasNeres <douglasneres06@gmail.com>
2025-01-29 16:52:39 -0300

    Changes in colors of dashboard

--- script.js
+++ script.js
@@ -33,15 +33,15 @@ document.querySelectorAll('.container').forEach((container) => {
         {
             label: 'Vendas',
             data: [10, 20, 5, 30, 4, 5, 20, 15, 40, 20, 30, 35],
-            backgroundColor: 'rgba(54, 162, 235, 0.5)',
-            borderColor: 'rgba(54, 162, 235, 1)',
+            backgroundColor: 'rgb(129, 129, 129)',
+            borderColor: 'rgb(30, 30, 30)',
             borderWidth: 1
         },
         {
             label: 'Cancelamentos',
             data: [5, 15, 30, 18, 10, 20, 3, 10, 20, 5, 10, 20],
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            borderColor: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(255, 47, 47, 0.5)',
+            borderColor: 'rgba(255, 0, 0, 0.5)',
             borderWidth: 1
         }
     ],
@@ -77,10 +77,10 @@ document.querySelectorAll('.container').forEach((container) => {
       datasets: [{
           label: 'Histórico de MRR',
           data: [10, 20, 15, 25, 10, 35, 25, 30, 15, 30, 40, 50], // Substitua pelos seus dados
-          borderColor: '#f6c23e', // Cor da linha
-          backgroundColor: 'rgba(246, 194, 62, 0.2)', // Cor de preenchimento abaixo da linha
+          borderColor: 'rgba(255, 47, 47, 0.5)', // Cor da linha
+          backgroundColor: 'rgba(255, 0, 0, 0.5)', // Cor de preenchimento abaixo da linha
           tension: 0.4, // Suaviza as curvas
-          pointBackgroundColor: '#f6c23e', // Cor dos pontos
+          pointBackgroundColor: 'rgba(255, 47, 47, 0.5)', // Cor dos pontos
           pointBorderWidth: 2,
           pointRadius: function(context) {
               // Destacar o maior ponto
